282d8ebb3ba68026e42feb0e50ea8be0960558fb
tugcan-y <tugcanyildiz.de@gmail.com>
2023-04-02 10:55:36 +0300

    new files added

---
+++ python-app2.py
@@ -0,0 +1,13 @@
+game = "witcher 3"
+print(game.endswith("4"))   #False
+sentence = "I live and work in Turkey"
+sentence = sentence.capitalize()
+print("I live and work in Turkey")
+print(sentence.capitalize())
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.title())
+print(sentence.replace("Turkey", "Japan"))
+print(sentence.swapcase())
+text = "tugcanyildiz.com"
+print(text.replace("s", "m")) # s ler m ye çevirildi.
